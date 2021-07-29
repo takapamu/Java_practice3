1158a6ab8067581e5b498bd16175f5c626137ff8
takapamu <takapamukyary0129@gmail.com>
2021-07-29 15:53:43 +0900

    add Math.log.java

---
+++ Math_log.java
@@ -0,0 +1,19 @@
+class Math_log {
+  public static void main(String[] args){
+    System.out.println(Math.log(10));
+    System.out.println(Math.log(1));
+    System.out.println(Math.log(Math.E));
+
+    System.out.println("-- --");
+
+    System.out.println(Math.log10(100));
+    System.out.println(Math.log10(10));
+    System.out.println(Math.log10(1));
+
+    System.out.println("-- --");
+
+    System.out.println(Math.log1p(10));
+    System.out.println(Math.log1p(1));
+    System.out.println(Math.log1p(0));
+  }
+}
