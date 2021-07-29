f93f2c2dcaf4de5b4179ade96d0a6826580b0609
takapamu <takapamukyary0129@gmail.com>
2021-07-29 15:34:50 +0900

    add Math_min.java

---
+++ Math_min.java
@@ -0,0 +1,10 @@
+class Math_min{
+  public static void main(String[] args)
+  {
+    int num1 = 20;
+    int num2 = 12;
+
+    System.out.println(Math.max(num1, num2));
+    System.out.println(Math.min(num1, num2));
+  }
+}
