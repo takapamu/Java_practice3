8004687ea0cb64495c027be42269a897e107329d
takapamu <takapamukyary0129@gmail.com>
2021-07-29 13:45:04 +0900

    add Absolute_value.java

---
+++ Absolute_value.java
@@ -0,0 +1,14 @@
+class Absolute_value {
+  public static void main(String[] args){
+    int i1 = 24;
+    int i2 = -18;
+
+    System.out.println(i1 + "->" + Math.abs(i1));
+    System.out.println(i2 + "->" + Math.abs(i2));
+
+    double d1 = 14.53;
+    double d2 = -7.224;
+    System.out.println(d1 + "->" + Math.abs(d1));
+    System.out.println(d2 + "->" + Math.abs(d2));
+  }
+}
