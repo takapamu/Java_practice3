180332299a91aeac7fc5979b017992eccd3a0787
takapamu <takapamukyary0129@gmail.com>
2021-07-29 21:01:12 +0900

    add Math_sin.java

---
+++ Math_sin.java
@@ -0,0 +1,10 @@
+class Math_sin {
+  public static void main(String[] args){
+    System.out.println("sin30° = " + Math.sin(Math.PI/6));
+    System.out.println("sin45° = " + Math.sin(Math.PI/4));
+    System.out.println("cos60° = " + Math.cos(Math.PI/3));
+    System.out.println("cos45° = " + Math.cos(Math.PI/4));
+    System.out.println("tan60° = " + Math.tan(Math.PI/3));
+    System.out.println("tan45° = " + Math.tan(Math.PI/4));
+  }
+}
