d1f99343ffb6a7b6ca1f455171f0ed8ba047a613
takapamu <takapamukyary0129@gmail.com>
2021-07-29 15:39:03 +0900

    add Math_pow.java

---
+++ Math_pow.java
@@ -0,0 +1,12 @@
+class Math_pow {
+  public static void main(String[] args){
+    double d = 3.0;
+
+    System.out.println(Math.pow(d, 0.0));
+    System.out.println(Math.pow(d, 1.0));
+    System.out.println(Math.pow(d, 2.0));
+    System.out.println(Math.pow(d, 3.0));
+    System.out.println(Math.pow(d, 4.0));
+    System.out.println(Math.pow(d, 5.0));
+  }
+}
