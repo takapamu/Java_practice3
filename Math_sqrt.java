dd8b69036072640e9c88f4a56b2aae29c4832274
takapamu <takapamukyary0129@gmail.com>
2021-07-29 16:00:58 +0900

    add Math_sqrt.java

---
+++ Math_sqrt.java
@@ -0,0 +1,12 @@
+class Math_sqrt {
+  public static void main(String[] args){
+    System.out.println("sqrt(16)=" + Math.sqrt(16));
+    System.out.println("sqrt(30)=" + Math.sqrt(30));
+    System.out.println("sqrt(-25)=" + Math.sqrt(-25));
+
+    System.out.println("sqrt(8)=" + Math.sqrt(8));
+    System.out.println("sqrt(40)=" + Math.sqrt(40));
+    System.out.println("sqrt(-64)=" + Math.sqrt(-64));
+
+  }
+}
