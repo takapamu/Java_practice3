1ffa6e64dcc954d305e58948a30d0d314ec6de40
takapamu <takapamukyary0129@gmail.com>
2021-07-31 23:04:33 +0900

    初期状態の配置

--- Board.java
+++ Board.java
@@ -13,10 +13,39 @@ public class Board{
   static String stone;
   static String rev_stone;
 
+  static public void initialize(){
+
   //オセロ版の要素を全てクリアする
-  for (int i = 0; i < 8; i++){
-    for (int j = 0; j < 8; j++){
-      board[i][j] = EMPTY;
+    for (int i = 0; i < 8; i++){
+      for (int j = 0; j < 8; j++){
+        board[i][j] = EMPTY;
+      }
     }
+
+    //初期状態の配置
+    board[3][3] = BLACK;
+    board[3][4] = WHITE;
+    board[4][3] = WHITE;
+    board[4][4] = BLACK;
+
+    //次打つ駒の色を指定
+    stone = BLACK;
+    rev_stone = WHITE;
+
+    //ゲーム実行中フラグ
+    game = true;
+  }
+
+  static public void showBoard(){
+
+    //まだ空いている座標はあるか
+    boolean existempty = false;
+
+    //黒い駒の数集計用
+    int cnt_black = 0;
+
+    //白い駒の数集計用
+    int cnt_white = 0;
+    
   }
 }
