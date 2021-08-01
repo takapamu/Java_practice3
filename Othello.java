36bc7d4295f86208e5e8b0fbdc2df3c4d1e76c36
takapamu <takapamukyary0129@gmail.com>
2021-08-01 21:49:40 +0900

    finish

--- Othello.java
+++ Othello.java
@@ -1,5 +1,25 @@
 import java.util.Scanner;
 
 public class Othello {
-  
+  public static void main(String[] args){
+
+    Board.initialize();
+    Board.showBoard();
+
+    //コンソールからの入力を受け取る
+    Scanner s = new Scanner(System.in);
+
+    //ゲーム実行中フラグがtrueのあいだループする
+    while(Board.game){
+
+      System.out.println("駒を置くx座標を入力してください: ");
+      int x = s.nextInt();
+
+      System.out.println("駒を置くy座標を入力してください: ");
+      int y = s.nextInt();
+
+      Board.setStone(x, y);
+    }
+    s.close();
+  }
 }
