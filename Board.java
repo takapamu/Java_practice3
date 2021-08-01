00a79dfbf57b3e5a784b5c92bfc36cf26fffda93
takapamu <takapamukyary0129@gmail.com>
2021-08-01 21:40:46 +0900

    finish

--- Board.java
+++ Board.java
@@ -156,4 +156,66 @@ public class Board{
       }
     }
   }
+
+  static public void turnLeftDown(int x, int y){
+    if(y < 6 && x > 1){
+
+      //となりの駒
+      String next = board[y + 1][x - 1];
+
+      // となりの駒が裏駒の場合
+      if (next.equals(rev_stone)) {
+
+      // さらにその一つとなりから順に確認
+        for (int i = 2; true; i++) {
+
+          if(x - i < 0 || y + i > 7 || board[y + i][x - i].equals(EMPTY)){
+            //駒がない場合終了
+            break;
+          }else if(board[y + i][x - i].equals(stone)){
+            //自駒の場合
+
+            //あいだの駒をすべて自駒にひっくりかえす
+            for(int t = 1; t < i; t++){
+
+              //配列の要素を上書き
+              board[y + t][x - t] = stone;
+            }
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  static public void turnRightDown(int x, int y){
+    if(y < 6 && x < 6){
+
+      //となりの駒
+      String next = board[y + 1][x + 1];
+
+      // となりの駒が裏駒の場合
+      if (next.equals(rev_stone)) {
+
+      // さらにその一つとなりから順に確認
+        for (int i = 2; true; i++) {
+
+          if(x + i > 7 || y + i > 7 || board[y + i][x + i].equals(EMPTY)){
+            //駒がない場合終了
+            break;
+          }else if(board[y + i][x + i].equals(stone)){
+            //自駒の場合
+
+            //あいだの駒をすべて自駒にひっくりかえす
+            for(int t = 1; t < i; t++){
+
+              //配列の要素を上書き
+              board[y + t][x + t] = stone;
+            }
+            break;
+          }
+        }
+      }
+    }
+  }
 }
