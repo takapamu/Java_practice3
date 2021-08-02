c6b5250110dca3e02d3d50eef4479113f89b1d7b
takapamu <takapamukyary0129@gmail.com>
2021-08-02 22:23:03 +0900

    finish

--- MainClass.java
+++ MainClass.java
@@ -4,32 +4,32 @@ import java.util.*;
 import javax.swing.JFrame;
 
 public class MainClass extends JFrame {
-private final static int WINDOW_SIZE = 300;
-private final static int MOLE_SIZE = 50;
-private final static int GAME_END_COUNT = 20;
-private static final String Over = null;
+  private final static int WINDOW_SIZE = 300;
+  private final static int MOLE_SIZE = 50;
+  private final static int GAME_END_COUNT = 20;
+  private static final String Over = null;
 
-private int score = 0;
-private int counter = 0;
-private int mole_x;
-private int mole_y;
-private boolean mole_hit = false;
+  private int score = 0;
+  private int counter = 0;
+  private int mole_x;
+  private int mole_y;
+  private boolean mole_hit = false;
 
 public static void main(String[] args) {
-new MainClass();
+  new MainClass();
 }
 
 public MainClass() {
-setSize(WINDOW_SIZE, WINDOW_SIZE);
+  setSize(WINDOW_SIZE, WINDOW_SIZE);
 
-// リスナー
-GameMouseAdapter adapter = new GameMouseAdapter();
-addMouseListener(adapter);
+  // リスナー
+  GameMouseAdapter adapter = new GameMouseAdapter();
+  addMouseListener(adapter);
 
-setVisible(true);
+  setVisible(true);
 
-Timer t = new Timer();
-t.schedule(new GameTimeTask(), 1000l, 1000l);
+  Timer t = new Timer();
+  t.schedule(new GameTimeTask(), 1000l, 1000l);
 }
 
 private class GameTimeTask extends TimerTask {
@@ -46,50 +46,50 @@ repaint();
 
 private class GameMouseAdapter extends MouseAdapter {
 @Override
-public void mouseClicked(MouseEvent e) {
-if (mole_hit) return; // モグラがやられている時は判定しない
-int x = e.getPoint().x;
-int y = e.getPoint().y;
-int h = MOLE_SIZE/2;
-int mx = mole_x + h;
-int my = mole_y + h;
-int x_k = x - mx;
-int y_k = y - my;
-if (x_k * x_k + y_k * y_k < h * h) {
-score++;
-mole_hit = true;
-}
-// 描画する
-repaint();
-}
+  public void mouseClicked(MouseEvent e) {
+    if (mole_hit) return; // モグラがやられている時は判定しない
+    int x = e.getPoint().x;
+    int y = e.getPoint().y;
+    int h = MOLE_SIZE/2;
+    int mx = mole_x + h;
+    int my = mole_y + h;
+    int x_k = x - mx;
+    int y_k = y - my;
+      if (x_k * x_k + y_k * y_k < h * h) {
+      score++;
+      mole_hit = true;
+      }
+  // 描画する
+  repaint();
+  }
 }
 
 public void paint(Graphics g) {
-g.drawImage(drawScreen(), 0, 0, this);
+  g.drawImage(drawScreen(), 0, 0, this);
 }
 
 private Image drawScreen() {
-Image screen = createImage(WINDOW_SIZE, WINDOW_SIZE);
-Graphics2D g = (Graphics2D)screen.getGraphics();
+  Image screen = createImage(WINDOW_SIZE, WINDOW_SIZE);
+  Graphics2D g = (Graphics2D)screen.getGraphics();
 
-// スコア表示
-g.setColor(Color.BLACK);
-g.drawString(“”SCORE：”” + score, 50, 50);
+  // スコア表示
+  g.setColor(Color.BLACK);
+  g.drawString("SCORE：" + score, 50, 50);
 
-if (counter<=GAME_END_COUNT) {
-g.drawString(“Game Over”, 100, 100);
-return screen;
-}
+  if (counter<=GAME_END_COUNT) {
+    g.drawString("Game Over", 100, 100);
+    return screen;
+  }
 
-// モグラ描画
-if (mole_hit)
-g.setColor(Color.RED);
-else
-g.setColor(Color.YELLOW);
-g.fillOval(mole_x, mole_y, MOLE_SIZE, MOLE_SIZE);
-g.setColor(Color.BLACK);
-g.drawOval(mole_x, mole_y, MOLE_SIZE, MOLE_SIZE);
+  // モグラ描画
+  if (mole_hit)
+    g.setColor(Color.RED);
+    else
+    g.setColor(Color.YELLOW);
+    g.fillOval(mole_x, mole_y, MOLE_SIZE, MOLE_SIZE);
+    g.setColor(Color.BLACK);
+    g.drawOval(mole_x, mole_y, MOLE_SIZE, MOLE_SIZE);
 
-return screen;
-}
+  return screen;
+  }
 }
